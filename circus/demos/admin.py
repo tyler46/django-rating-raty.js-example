1fa1d1f707d11529c9eb76caf92b42356df259ab
tyler46 <mail.doctor46@gmail.com>
2013-05-15 23:35:07 +0300

    Add rating model in demo app.

--- circus/demos/admin.py
+++ circus/demos/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from .models import Show
+from .models import Rating, Show
 
 
-admin.site.register(Show)
+class RatingInline(admin.StackedInline):
+    """Rating Inline."""
+    model = Rating
+    extra = 1
+
+
+class ShowAdmin(admin.ModelAdmin):
+    inlines = [RatingInline]
+    model = Show
+    exclude = ('slug', )
+    list_display = ('title', 'description', )
+
+
+admin.site.register(Show, ShowAdmin)
--- circus/demos/models.py
+++ circus/demos/models.py
@@ -1,3 +1,5 @@
+import django.utils.timezone as timezone
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import pre_save
@@ -6,7 +8,16 @@ from django.dispatch import receiver
 from uuslug import uuslug as slugify
 
 
+RATING_CHOICES = (
+    (1, 'bad'),
+    (2, 'poor'),
+    (3, 'regular'),
+    (4, 'good'),
+    (5, 'gorgeus'),)
+
+
 class Show(models.Model):
+    """Show model."""
     title = models.CharField(max_length=50, default='',
                              blank=True, null=False)
     slug = models.SlugField(blank=True, max_length=50)
@@ -23,6 +34,23 @@ class Show(models.Model):
 
     class Meta:
         ordering = ['start']
+
+
+class Rating(models.Model):
+    """Rating model for Show model."""
+    show = models.ForeignKey('Show', related_name='rated_show')
+    user = models.ForeignKey(User, related_name='user_rated')
+    rating = models.IntegerField(choices=RATING_CHOICES, default=1)
+    date_rated = models.DateTimeField(editable=False)
+
+    def __unicode__(self):
+        return u'%s rated %s (%s)' % (self.user, self.show,
+                                      self.get_rating_display())
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self_date_rated = timezone.now()
+        super(Rating, self).save(*args, **kwargs)
 
 
 @receiver(pre_save, sender=Show, dispatch_uid='create_slug_signal')
